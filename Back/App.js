85d37464fde988454ece257f192a6a461cacec46
nolan777 <florianbique@yahoo.fr>
2021-04-07 13:39:43 +0200

    add database connection

--- Back/App.js
+++ Back/App.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require('dotenv').config();
+const { Pool, Client } = require('pg')
 
 app.use(cors());
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_BASE,
+  password: process.env.DB_PASS,
+  port: 3211,
+})
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_BASE,
+    password: process.env.DB_PASS,
+    port: 3211,
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 app.listen(8010, () => console.log("server is running"));
