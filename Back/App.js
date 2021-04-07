a48187354265288463f6931fa878ec6ffcfe75a4
nolan777 <florianbique@yahoo.fr>
2021-04-07 16:23:05 +0200

    update bdd query

--- Back/App.js
+++ Back/App.js
@@ -4,20 +4,18 @@ const cors = require("cors");
 if (process.env.ENV_PROD !== true) {
     require('dotenv').config();   
 }
-const { Pool, Client } = require('pg')
+const { Client } = require('pg')
 
 app.use(cors());
 
-const pool = new Pool(process.env.DATABASE_URL)
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
 const client = new Client(process.env.DATABASE_URL)
 client.connect()
+//exemple de requete
 client.query('SELECT NOW()', (err, res) => {
   console.log(err, res)
   client.end()
 })
+
+
 
 app.listen(8010, () => console.log("server is running"));
