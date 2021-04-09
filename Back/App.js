b3e7bfe1132ac8a720d7cd8b899205643a8f55ba
nolan777 <florianbique@yahoo.fr>
2021-04-09 13:45:16 +0200

    merge save response

--- Back/App.js
+++ Back/App.js
@@ -8,7 +8,6 @@ const { Client } = require('pg')
 
 app.use(cors());
 const connectionString = process.env.DATABASE_URL;
-console.log(connectionString);
 
 const client = new Client({connectionString, ssl: { rejectUnauthorized: false }})
 client.connect()
@@ -18,6 +17,23 @@ const ReadResponse = client.query('SELECT * FROM response', (err, res) => {
 
 app.get("/responses", (req, res) => {
   res.json(ReadResponse.fields);
+})
+
+app.post("/responses", async (req, res) => {
+  console.log(req);
+  const { nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude } = req;
+
+  await client.query(
+    'INSERT INTO response (nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, creneau_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)',
+    [nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude],
+    (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    }
+  )
+  res.json('all good !');
 })
 
 app.listen(8010, () => console.log("server is running"));
