dfe3d5f8b4b4311a9b7c5fd741a6311946be5bf7
Yoann <yo.cote69@gmail.com>
2021-04-09 16:59:09 +0200

    cors

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueAnime from 'vue-animejs';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  VueAnime,
   render: h => h(App)
 }).$mount('#app')
