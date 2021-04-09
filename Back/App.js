4b7e391e44be8b796df63bcc6f2bf05b97461af6
nolan777 <florianbique@yahoo.fr>
2021-04-09 14:02:35 +0200

    update clean query

--- Back/App.js
+++ Back/App.js
@@ -3,34 +3,31 @@ const app = express();
 const cors = require("cors");
 const https = require('https');
 const joi = require('joi');
-if (process.env.ENV_PROD !== true) {
+const excel = require('node-excel-export');
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();   
 }
-const { Pool, Client } = require('pg')
+const { Client } = require('pg')
 
 app.use(cors());
-
 const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool(process.env.DATABASE_URL)
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-const client = new Client(process.env.DATABASE_URL)
+const client = new Client({connectionString, ssl: { rejectUnauthorized: false }})
 client.connect()
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
+
+const ReadResponse = client.query('SELECT * FROM response', (err, res) => {
 })
 
-//route qui permet de modifier les evenements
+app.get("/responses", (req, res) => {
+  res.json(ReadResponse.fields);
+})
+
 app.put("/events/:id", ()=>{
   //on récupère les données à enregistrer
   const body = req.body;
 
   //on enregistre les events en base
-  client.query("UPDATE events SET libelle = body.libelle  AND date = body.date WHERE id =req.params.id", (err,res)=>{
+  client.query("UPDATE events SET libelle = '"+body.libelle+"'  AND date = '"+body.date+"' WHERE id =req.params.id", (err,res)=>{
     if(err){
       return res.json({error: "Problème lors de la modification de l'évènement"});
     }
@@ -50,20 +47,18 @@ app.delete("/events/:id", ()=>{
   });
 });
 
-
 //route qui permet de modifier les evenements
 app.post("/events", ()=>{
   //on récupère les données à enregistrer
   const body = req.body;
 
   //on enregistre les events en base
-  client.query("INSERT INTO events (libelle,date) VALUES (body.libelle,body.date)", (err,res)=>{
+  client.query("INSERT INTO events (libelle,date) VALUES ('"+body.libelle+"','"+body.date+"')", (err,res)=>{
     if(err){
       return res.json({error: "Problème lors de l'ajout de l'évènement"});
     }
   });
 });
-
 
 app.post("/response", ()=>{
   //on récupère les données à enregistrer
@@ -73,17 +68,16 @@ app.post("/response", ()=>{
   client.query("SELECT email FROM response WHERE EXISTS (SELECT email FROM response WHERE email ='"+body.email+"'",(err,res)=>{
     if(res){
       //si on a un résultat on retourne un message : l'email est déjà utilisé
-      return res.json({errorMail: "Cet email est déjà utilisé"});
+      return res.json({error: "Cet email est déjà utilisé"});
     }
   });
 
-  //on fait les vérifications sur les informations récupérées
+  //on fait les vérifications sur les informations récupérées avec joi
   const reponses = joi.object({
-
     email : joi.string().email.required()
   });
-  //on vérifie l'email
-  const {error} = reponses.validate(body.email);
+  //on utilise la fonction validate pour vérifier les données
+  const {error} = reponses.validate(body);
   if(error){
     return res.json({error: error.details});
   }
@@ -94,20 +88,51 @@ app.post("/response", ()=>{
         console.log(data);
     });
     response.on('error',(error)=>{
-      return res.json({errorMail: "Ce numéro de téléphone n'est pas valide"});
+      return res.json({error: "Ce numéro de téléphone n'est pas valide"});
     });
   }).on('error', console.error);
 
   //on enregistre les données en base
-  client.query("INSERT INTO response () VALUES ()", (err,res)=>{
+  client.query("INSERT INTO response (nom,prenom,naissance,civilite,tel,fixe,email,cp,ville,niveau_actuel,code_annee,souhait_contact,formation_souhaitee,is_initial,code_formation,creneau_journalier,creneau_horaire,canal_acquisition,note_echange,etudiant_spe,ville_etude) VALUES (,,,,,,,,,,,,,,,,,,,,study_city)", (err,res)=>{
     if(err){
-      return res.json({errorMail: "Problème lors de l'ajout des réponses de l'utilisateur"});
+      return res.json({error: "Problème lors de l'ajout des réponses de l'utilisateur"});
     }
   });
 });
 
+//fonction excel
+app.get('/excel', ()=>{
 
-app.post("/responses", async (req, res) => {
+
+  //on ajoute les données à dataset
+  const dataset = ?;
+
+  //
+  const merges = [
+    { start: { row: 1, column: 1 }, end: { row: 1, column: 10 } },
+    { start: { row: 2, column: 1 }, end: { row: 2, column: 5 } },
+    { start: { row: 2, column: 6 }, end: { row: 2, column: 10 } }
+  ]
+
+  //on créé l'export
+  const report = excel.buildExport(
+    [
+      {
+        name: 'Report',
+        heading: heading,
+        merges: merges,
+        specification: specification,
+        data: dataset
+      }
+    ]
+  );
+
+  //retourne le fichier
+  res.attachment('.xlsx');
+  return res.send(report)
+});
+
+/*app.post("/responses", async (req, res) => {
   console.log(req);
   const { nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude } = req;
 
@@ -122,6 +147,6 @@ app.post("/responses", async (req, res) => {
     }
   )
   res.json('all good !');
-})
+})*/
 
 app.listen(8010, () => console.log("server is running"));
