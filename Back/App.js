9b619e908815ccfa2ccb98de398d922575398f32
nolan777 <florianbique@yahoo.fr>
2021-04-13 15:27:26 +0200

    update, add: correction des routes + ajout authentification

--- Back/App.js
+++ Back/App.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const https = require('https');
 const joi = require('joi');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();   
 }
@@ -15,100 +17,136 @@ const connectionString = process.env.DATABASE_URL;
 const client = new Client({connectionString, ssl: { rejectUnauthorized: false }})
 client.connect()
 
-const ReadResponse = client.query('SELECT * FROM response', (err, res) => {
-})
-
 app.get("/responses", (req, res) => {
-  res.json(ReadResponse.fields);
+  client.query(`SELECT * FROM response`, (err, result) => {
+    const responseFound = result.rows;
+    return res.json(responseFound);
+  });
 })
 
 //route qui permet de modifier les evenements
-app.put("/events/:id", ()=>{
-  //on récupère les données à enregistrer
+app.put("/events/:id", (req, res)=>{
   const body = req.body;
-
-  //on enregistre les events en base
-  client.query("UPDATE events SET libelle = body.libelle  AND date = body.date WHERE id =req.params.id", (err,res)=>{
+  //on modifie l'event en base
+  client.query("UPDATE events SET libelle = '"+body.libelle+"' AND date = '"+body.date+"' WHERE id = "+req.params.id, (err)=>{
     if(err){
       return res.json({error: "Problème lors de la modification de l'évènement"});
+    } else {
+      return res.json({message: "Evènement modifié"});
     }
   });
 });
 
-//route qui permet de supprimer les evenements
-app.delete("/events/:id", ()=>{
-  //on récupère les données à enregistrer
-  const body = req.body;
-
-  //on enregistre les events en base
-  client.query("DELETE FROM events WHERE id =req.params.id", (err,res)=>{
+//route qui permet de supprimer un évènement
+app.delete("/events/:id", (req, res)=>{
+  client.query("DELETE FROM event WHERE id = "+req.params.id, (err)=>{
+    console.log(err);
     if(err){
       return res.json({error: "Problème lors de la suppression de l'évènement"});
+    } else {
+      return res.json({message: "Evènement supprimé"});
     }
   });
 });
 
-
-//route qui permet de modifier les evenements
-app.post("/events", ()=>{
-  //on récupère les données à enregistrer
+//route qui permet de modifier les évènements
+app.post("/events", (req, res)=>{
   const body = req.body;
-
-  //on enregistre les events en base
-  client.query("INSERT INTO events (libelle,date) VALUES (body.libelle,body.date)", (err,res)=>{
+  //on enregistre l'event en base
+  client.query("INSERT INTO event (libelle_event, date_event, is_visible) VALUES ($1, $2, $3)", [body.libelle, body.date, 1], (err)=>{
     if(err){
+      console.log(err);
       return res.json({error: "Problème lors de l'ajout de l'évènement"});
+    } else {
+      return res.json({message: "Evénement enregistré"});
     }
   });
 });
 
-
-app.post("/response", ()=>{
-  //on récupère les données à enregistrer
+app.post("/response", (req, res)=>{
   const body = req.body;
-  
-  //on fait un test d'unicité sur l'email avec une requête existe
-  client.query("SELECT email FROM response WHERE EXISTS (SELECT email FROM response WHERE email ='"+body.email+"'",(err,res)=>{
-    if(res){
+  //on fait un test d'unicité sur l'email avec une requête WHERE
+  client.query(`SELECT email FROM response WHERE email = '`+body.email+`'`, (err, result)=>{
+    if(result.rows[0]){
       //si on a un résultat on retourne un message : l'email est déjà utilisé
       return res.json({errorMail: "Cet email est déjà utilisé"});
-    }
-  });
+    } else {
+      //on fait les vérifications sur les informations récupérées
+      const reponses = joi.object({
+        email : joi.string().email().required()
+      });
+      const mail = {email: body.email}
+      //on vérifie l'email
+      const {error} = reponses.validate(mail);
+      
+      if(error){
+        //si il y a une erreur, on la retourne
+        return res.json({error: error.details});
+      } else {
+        //on vérifie le numéro de téléphone
+        https.get('https://numvalidate.com/api/validate?number='+body.phone,(response)=>{
+          response.setEncoding("utf8");
+          response.on('data', ()=>{
+            //on enregistre les données en base
+            const { nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude } = req.body;
+            client.query(
+              'INSERT INTO response (nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, creneau_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21)',
+              [nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude],
+              (err) => {
+                console.log(err);
+                if (err) {
+                  return res.json({errorMail: "Problème lors de l'ajout des réponses de l'utilisateur"});
 
-  //on fait les vérifications sur les informations récupérées
-  const reponses = joi.object({
-
-    email : joi.string().email.required()
-  });
-  //on vérifie l'email
-  const {error} = reponses.validate(body.email);
-  if(error){
-    return res.json({error: error.details});
-  }
-  //on vérifie le téléphone
-  https.get('https://numvalidate.com/api/validate?number='+body.phone,(response)=>{
-    response.setEncoding("utf8");
-    response.on('data', (data)=>{
-        console.log(data);
-    });
-    response.on('error',(error)=>{
-      return res.json({errorMail: "Ce numéro de téléphone n'est pas valide"});
-    });
-  }).on('error', console.error);
-
-  //on enregistre les données en base
-  const { nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude } = req;
-
-  await client.query(
-    'INSERT INTO response (nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, creneau_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)',
-    [nom, prenom, naissance, civilite, tel, fixe, email, cp, ville, niveau_actuel, code_annee, souhait_contact, formation_souhaitee, is_initial, code_formation, creneau_journalier, crenaeu_horaire, canal_acquisition, note_echange, etudiant_spe, ville_etude],
-    (err, res) => {
-      if (err) {
-        return res.json({errorMail: "Problème lors de l'ajout des réponses de l'utilisateur"});
-
+                } else {
+                  return res.json({message: "Réponse enregistée"});
+                }
+              }
+            );
+          });
+          response.on('error',(error)=>{
+            return res.json({errorMail: "Ce numéro de téléphone n'est pas valide"});
+          });
+        }).on('error', console.error);
       }
     }
-  ); 
+  });
 });
+
+//authentification
+
+//fonction qui génère un Token d'accès
+function generateAccessToken(user){
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3h'})
+}
+//route qui permet l'authentification
+app.post("/login", async (req, res) =>{
+  const body = req.body;
+  client.query(`SELECT mail, password FROM users WHERE users.mail = '`+body.mail+`'`, async (err, result) => {
+    const userFound = result.rows[0];
+
+    const mail = body.mail;
+    const user = { mail: mail };
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+
+    if (!userFound) {
+      //si les informations ne correspondent pas, on retourne un message d'erreur
+      return res.json({errmessage: 'Nom d\'utilisateur / mot de passe incorrect'})
+    }
+    try {
+      const isSame = await bcrypt.compare(body.password, userFound.password);
+      if ( isSame ) {
+        //si le mot de passe correspond, on authentifie l'utilisateur
+        client.query('INSERT INTO refresh_tokens (refresh_token) VALUES ($1)', [refreshToken], (err, res) => {});
+        res.json({token: accessToken, refreshToken: refreshToken, user: userFound, mail: userFound.mail});
+      } else {
+        //si le mot de passe ne correspond pas, on retourne un message d'erreur
+        return res.json({errmessage: 'Nom d\'utilisateur / mot de passe incorrect'})
+      }
+    } catch {
+      res.status(500).send('Problème serveur')
+    } 
+  });
+})
 
 app.listen(8010, () => console.log("server is running"));
