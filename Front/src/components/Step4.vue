a38a069970527e3b1ba835bfc2e25bdf1bb688df
nolan777 <florianbique@yahoo.fr>
2021-04-24 21:40:58 +0200

    update fixing step4

--- Front/src/components/Step4.vue
+++ Front/src/components/Step4.vue
@@ -1,12 +1,14 @@
 <template>
 <div id="step4">
-   <div id="text">Merci d'avoir rempli notre formulaire</div>
-  <v-img id="plane"
+   <div id="text">Merci d'avoir rempli notre formulaire !</div>
+  <v-img id="plane" v-on:click="show = !show"
   size="large"
             max-height="300"
   max-width="303"
           src="../assets/paperplane.png"
         ></v-img>
+        <transition name="fade">
+  </transition>
  
 </div>
 
@@ -17,7 +19,9 @@
 export default {
     name: "Step4",
   data() {
-    return {};
+    return {
+show:true
+    }
   },
   
 }
@@ -27,11 +31,24 @@ export default {
 #text{
   font-weight: bold;
   font-size: 25px;
-  margin-left:12rem;
+  margin-left:10rem;
   display: flex !important;
+  justify-content:space-around;
+  font-family: 'Karla', sans-serif;
+  max-width: 100%;
+    height: auto;
 }
 #plane{
-
+margin-left: 5rem;
+margin-top: 2rem;
+max-width: 100%;
+    height: auto;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 
 </style>
